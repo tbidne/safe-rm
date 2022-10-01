d1353b9935840c6b00f02f4be06dd0b71afb64c8
Tommy Bidne <tbidne@protonmail.com>
2022-10-01 20:23:15 +1300

    Move exe-internal to libs; add file-utils

---
+++ lib/file-utils/SafeRm/FileUtils.hs
@@ -0,0 +1,108 @@
+-- | Provides utils for file system assertions.
+--
+-- @since 0.1
+module SafeRm.FileUtils
+  ( -- * File System Operations
+    createFiles,
+    createFileContents,
+    createDirectories,
+    clearDirectory,
+
+    -- ** Text
+    TextMatch (..),
+    matches,
+    unlineMatches,
+  )
+where
+
+import Data.ByteString qualified as BS
+import Data.Text qualified as T
+import SafeRm.Prelude hiding (IO)
+import System.IO (IO)
+import UnliftIO.Directory qualified as Dir
+
+-- | Creates empty files at the specified paths.
+--
+-- @since 0.1
+createFiles :: [FilePath] -> IO ()
+createFiles paths = for_ paths $ \p -> BS.writeFile p ""
+
+-- | Creates files at the specified paths.
+--
+-- @since 0.1
+createFileContents :: [(FilePath, ByteString)] -> IO ()
+createFileContents paths = for_ paths (uncurry BS.writeFile)
+
+-- | Creates empty files at the specified paths.
+--
+-- @since 0.1
+createDirectories :: [FilePath] -> IO ()
+createDirectories paths =
+  for_ paths $ \p -> Dir.createDirectoryIfMissing True p
+
+-- | Clears a directory by deleting it if it exists and then recreating it.
+--
+-- @since 0.1
+clearDirectory :: FilePath -> IO ()
+clearDirectory path = do
+  exists <- Dir.doesDirectoryExist path
+  when exists $ Dir.removePathForcibly path
+  createDirectoryIfMissing False path
+
+-- | Data type used for testing text matches.
+--
+-- @since 0.1
+data TextMatch
+  = Exact !Text
+  | Prefix !Text
+  | Infix !Text
+  | Suffix !Text
+  | Outfix !Text !Text
+  deriving stock
+    ( -- | @since 0.1
+      Eq,
+      -- | @since 0.1
+      Show
+    )
+
+matches :: [TextMatch] -> [Text] -> Maybe String
+matches [] [] = Nothing
+matches s@(_ : _) [] =
+  Just $ "Empty result but non-empty expectations: " <> show s
+matches [] t@(_ : _) =
+  Just $ "Empty expectations but non-empty result: " <> show t
+matches (e : es) (t : ts) = isMatch (e :| es) (t :| ts)
+
+isMatch :: NonEmpty TextMatch -> NonEmpty Text -> Maybe String
+isMatch (s :| es) (r :| rs) =
+  if isMatchHelper s (T.strip r)
+    then matches es rs
+    else
+      Just $
+        mconcat
+          [ "Expected: ",
+            showTextMatch s,
+            "\nReceived: ",
+            T.unpack r
+          ]
+
+isMatchHelper :: TextMatch -> Text -> Bool
+isMatchHelper (Exact e) r = e == r
+isMatchHelper (Prefix e) r = e `T.isPrefixOf` r
+isMatchHelper (Infix e) r = e `T.isInfixOf` r
+isMatchHelper (Suffix e) r = e `T.isSuffixOf` r
+isMatchHelper (Outfix e1 e2) r = e1 `T.isPrefixOf` r && e2 `T.isSuffixOf` r
+
+unlineMatches :: [TextMatch] -> String
+unlineMatches [] = ""
+unlineMatches (t : ts) = showTextMatch t <> "\n" <> unlineMatches ts
+
+showTextMatch :: TextMatch -> String
+showTextMatch (Exact e) = T.unpack e
+showTextMatch (Prefix e) = T.unpack e <> wc
+showTextMatch (Infix e) = wc <> T.unpack e <> wc
+showTextMatch (Suffix e) = wc <> T.unpack e
+showTextMatch (Outfix e1 e2) = T.unpack e1 <> wc <> T.unpack e2
+
+wc :: String
+wc = "**"
--- test/functional/Functional/Prelude.hs
+++ test/functional/Functional/Prelude.hs
@@ -8,31 +8,24 @@ module Functional.Prelude
     runSafeRm,
     captureSafeRm,
 
-    -- * File System Operations
-    createFiles,
-    createFileContents,
-    createDirectories,
-    clearDirectory,
-
     -- * Assertions
     assertFilesExist,
     assertFilesDoNotExist,
     assertDirectoriesExist,
     assertDirectoriesDoNotExist,
 
-    -- ** Text
-    TextMatch (..),
+    -- * Text assertion
     assertMatches,
     assertExceptionMatches,
   )
 where
 
 import Control.Monad.Reader (MonadReader (ask), ReaderT (ReaderT), runReaderT)
-import Data.ByteString qualified as BS
 import Data.List qualified as L
 import Data.Text qualified as T
 import SafeRm.Effects.Terminal (Terminal (putStr, putStrLn))
 import SafeRm.Exceptions (ExceptionI, ExceptionIndex (SomeExceptions))
+import SafeRm.FileUtils as X
 import SafeRm.Prelude as X hiding (IO)
 import SafeRm.Runner qualified as Runner
 import System.IO as X (IO)
@@ -92,34 +85,6 @@ captureSafeRm argList = do
   where
     funcIO = SysEnv.withArgs argList Runner.runSafeRm
 
--- | Creates empty files at the specified paths.
---
--- @since 0.1
-createFiles :: [FilePath] -> IO ()
-createFiles paths = for_ paths $ \p -> BS.writeFile p ""
-
--- | Creates files at the specified paths.
---
--- @since 0.1
-createFileContents :: [(FilePath, ByteString)] -> IO ()
-createFileContents paths = for_ paths (uncurry BS.writeFile)
-
--- | Creates empty files at the specified paths.
---
--- @since 0.1
-createDirectories :: [FilePath] -> IO ()
-createDirectories paths =
-  for_ paths $ \p -> Dir.createDirectoryIfMissing True p
-
--- | Clears a directory by deleting it if it exists and then recreating it.
---
--- @since 0.1
-clearDirectory :: FilePath -> IO ()
-clearDirectory path = do
-  exists <- Dir.doesDirectoryExist path
-  when exists $ Dir.removePathForcibly path
-  createDirectoryIfMissing False path
-
 -- | Asserts that files exist.
 --
 -- @since 0.1
@@ -156,22 +121,6 @@ assertDirectoriesDoNotExist paths =
     exists <- Dir.doesDirectoryExist p
     assertBool ("Expected directory not to exist: " <> p) (not exists)
 
--- | Data type used for testing text matches.
---
--- @since 0.1
-data TextMatch
-  = Exact !Text
-  | Prefix !Text
-  | Infix !Text
-  | Suffix !Text
-  | Outfix !Text !Text
-  deriving stock
-    ( -- | @since 0.1
-      Eq,
-      -- | @since 0.1
-      Show
-    )
-
 -- | Tests text for matches.
 --
 -- @since 0.1
@@ -204,45 +153,3 @@ assertExceptionMatches s exs = do
       assertMatches s (L.sort hs)
   where
     exTxt = T.lines . T.pack $ displayException exs
-
-matches :: [TextMatch] -> [Text] -> Maybe String
-matches [] [] = Nothing
-matches s@(_ : _) [] =
-  Just $ "Empty result but non-empty expectations: " <> show s
-matches [] t@(_ : _) =
-  Just $ "Empty expectations but non-empty result: " <> show t
-matches (e : es) (t : ts) = isMatch (e :| es) (t :| ts)
-
-isMatch :: NonEmpty TextMatch -> NonEmpty Text -> Maybe String
-isMatch (s :| es) (r :| rs) =
-  if isMatchHelper s (T.strip r)
-    then matches es rs
-    else
-      Just $
-        mconcat
-          [ "Expected: ",
-            showTextMatch s,
-            "\nReceived: ",
-            T.unpack r
-          ]
-
-isMatchHelper :: TextMatch -> Text -> Bool
-isMatchHelper (Exact e) r = e == r
-isMatchHelper (Prefix e) r = e `T.isPrefixOf` r
-isMatchHelper (Infix e) r = e `T.isInfixOf` r
-isMatchHelper (Suffix e) r = e `T.isSuffixOf` r
-isMatchHelper (Outfix e1 e2) r = e1 `T.isPrefixOf` r && e2 `T.isSuffixOf` r
-
-unlineMatches :: [TextMatch] -> String
-unlineMatches [] = ""
-unlineMatches (t : ts) = showTextMatch t <> "\n" <> unlineMatches ts
-
-showTextMatch :: TextMatch -> String
-showTextMatch (Exact e) = T.unpack e
-showTextMatch (Prefix e) = T.unpack e <> wc
-showTextMatch (Infix e) = wc <> T.unpack e <> wc
-showTextMatch (Suffix e) = wc <> T.unpack e
-showTextMatch (Outfix e1 e2) = T.unpack e1 <> wc <> T.unpack e2
-
-wc :: String
-wc = "**"
